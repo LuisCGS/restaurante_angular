0595261b499d9b5ffd5dcc6f9f0cb53243efd16d
Luis Claudio G. Sanches <luis.sanches@sigma.com.br>
2018-06-04 10:30:43 -0300

    Projeto em Angular - Restaurante

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ROUTES } from './app.routes';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantDetailComponent } from './restaurants/restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurants/restaurant-detail/menu/menu.component'
+import { MenuItemComponent } from './restaurants/restaurant-detail/menu-item/menu-item.component';
+import { ShoppingCartComponent } from './restaurants/restaurant-detail/shopping-cart/shopping-cart.component';
+import { ReviewsComponent } from './restaurants/restaurant-detail/reviews/reviews.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		HomeComponent,
+		RestaurantsComponent,
+		RestaurantComponent,
+		RestaurantDetailComponent,
+		MenuComponent,
+		ShoppingCartComponent,
+		MenuItemComponent,
+		ReviewsComponent,
+		OrderSummaryComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpModule,
+		SharedModule,
+		CoreModule,
+		RouterModule.forRoot(ROUTES)
+	],
+	providers: [CoreModule, { provide: LOCALE_ID, useValue: 'pt-BR' }],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
